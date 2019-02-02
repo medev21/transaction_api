b4d4e3deb4456c0b689d6e76ee8667f1d24c93e0
medev21 <benmarteen07@gmail.com>
2019-02-02 17:28:31 -0500

    add create method in User controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,24 @@ module Api
                 user = User.find(params[:id])
                 render json: {user: user}, status: :ok
             end
-        
+
+            def create
+                user = User.new(user_params)
+
+                if user.save()
+                    render json: {message: "User Created!", user: user}, status: :ok
+                else
+                    render json: {message: "ERROR - User not created"}, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            #only accept these three parameters in the POST request, reject extra params
+            def user_params
+                params.permit(:first_name, :last_name, :email)
+            end
+
         end
     end
 end
