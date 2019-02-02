3399298521ce3710c916b3bb54faead238af85c6
medev21 <benmarteen07@gmail.com>
2019-02-02 16:28:32 -0500

    basic index for all users

--- app/controllers/api/v1/users_controllers.rb
+++ app/controllers/api/v1/users_controllers.rb
@@ -1,7 +1,12 @@
 module API
     module V1
-
+        class UsersController < ApplicationController
         
-
+            def index
+                users = User.all();
+                render json: {status: 200, count: users.length, data: users}, status: ok
+            end
+        
+        end
     end
 end
